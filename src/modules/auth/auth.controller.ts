3351358a831e9384ac04c91b200705e44ed85b50
manhhiep <doanmanhhiep.2003@gmail.com>
2026-06-29 16:08:19 +0700

    feat: refactor authentication DTOs and improve login response structure

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,31 +1,25 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from 'src/modules/auth/auth.service';
-import { LoginRequestDto } from 'src/modules/auth/dtos/login.request.dto';
-import { RegisterRequestDto } from 'src/modules/auth/dtos/register.request.dto';
+import { LoginRequestDto } from 'src/modules/auth/dtos/request/login.request.dto';
+import { LoginResponseDto } from 'src/modules/auth/dtos/response/login.response.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() body: LoginRequestDto) {
-    const result = await this.authService.login(body);
-
-    return {
-      status: 'OK',
-      message: 'Login successfully',
-      result,
-    };
+  async login(@Body() body: LoginRequestDto): Promise<LoginResponseDto> {
+    return this.authService.login(body);
   }
 
-  @Post('register')
-  async register(@Body() body: RegisterRequestDto) {
-    const result = await this.authService.register(body);
+  // @Post('register')
+  // async register(@Body() body: RegisterRequestDto) {
+  //   const result = await this.authService.register(body);
 
-    return {
-      status: 'OK',
-      message: 'Register successfully',
-      result,
-    };
-  }
+  //   return {
+  //     status: 'OK',
+  //     message: 'Register successfully',
+  //     result,
+  //   };
+  // }
 }
